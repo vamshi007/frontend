db999856f8349d3a760f4a1b2196a2189bc3fee6
vamshi007 <vamshivamshi148@gmail.com>
2024-03-03 20:47:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/'); // Replace with your URL
+        if (!response.ok) {
+          throw new Error('Network response was not ok.');
+        }
+        const fetchedData = await response.json();
+        setData(fetchedData);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User Data:</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            <strong>ID:</strong> {item.id}<br />
+            <strong>User Name:</strong> {item.user_name}<br />
+            <strong>First Name:</strong> {item.first_name}<br />
+            <strong>Last Name:</strong> {item.last_name}<br />
+            <br />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
